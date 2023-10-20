624b9e5b9b1eba505e99e898a38af19257b28e30
DanKos22 <G00397054@atu.ie>
2023-10-20 15:05:31 +0100

    Added code

--- src/main/java/ie/atu/notificationservice/NotificationController.java
+++ src/main/java/ie/atu/notificationservice/NotificationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NotificationController {
 
-    @PostMapping("/notify")
+    @PostMapping("/register")
     public String notifyDetails(@RequestBody UserDetails user){
         String notificationMessage = String.format("User %s (%s) has been successfully registered.", user.getName(), user.getEmail());
         return notificationMessage;
